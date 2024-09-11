54d8502054638e27c78bb95278e726db931e2f50
Douglas Caetano <douglas.cto.lima@gmail.com>
2024-09-11 17:59:06 -0300

    feat: add method to deposit, withdraw, statement and exit

--- app_naja_bank.py
+++ app_naja_bank.py
@@ -21,14 +21,46 @@ while True:
 
     opcao = input(menu)
 
-    if opcao == "1":
-        print("opcao 1 - depositar")
-    elif opcao == "2":
-        print("opcao 2 - sacar")
-    elif opcao == "3":
-        print("opcao 3 - extrato")
-    elif opcao == "0":
-        print("opcao 0 - sair")
+    if opcao == "1": # Depositar
+
+        valor = float(input("\nInforme o valor do Déposito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R${valor:.2f}\n"
+        else:
+            print("\nOps! O valor informado é inválido.")
+
+    elif opcao == "2": # Sacar
+
+        valor = float(input("\nInforme o valor do Saque: "))
+
+        excedeu_valor_saldo = valor > saldo
+        excedeu_valor_limite = valor > limite
+        excedeu_valor_saques = numero_saques >= LIMITE_SAQUES
+
+        if excedeu_valor_saldo:
+            print("\nOps! Saldo insuficiente.")
+        elif excedeu_valor_limite:
+            print("\nOps! Saque excedeu o limite.")
+        elif excedeu_valor_saques:
+            print("\nOps! Número máximo de saques atingido.")
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R${valor:.2f}\n"
+            numero_saques += 1
+        else:
+            print("\nOps! O valor informado é inválido.")
+
+    elif opcao == "3": # Extrato
+
+        print("\n---------------- Info. Extrato ----------------")
+        print("Não foram realizadas transações.\n" if not extrato else extrato)
+        print(f"Saldo: R${saldo:.2f}")
+        print("-----------------------------------------------")
+
+    elif opcao == "0": # Sair
+        print("Obrigado por utilizar o Naja Bank\n")
         break
     else:
         print("\nOperação inválida, digite novamente a opção desejada.")
